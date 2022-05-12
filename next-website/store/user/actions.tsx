4b0f3e00217ad8b1b06ebaf24727996d3a32a2cd
tianye <348086239@qq.cpm>
2022-05-12 13:05:02 +0800

    fix: !

--- next-website/store/user/actions.tsx
+++ next-website/store/user/actions.tsx
@@ -1,4 +1,4 @@
-import { constants } from './constants';
+import constants from './constants';
 import { login } from '@api/controller/loginController';
 import { message } from 'antd';
 import { ThunkResult } from '../interface/index';
