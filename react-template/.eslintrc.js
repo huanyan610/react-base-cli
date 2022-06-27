4a21ac80956e6f0d010a19f4967d591c46435d1a
huanyan <348086239@qq.com>
2022-06-27 16:48:53 +0800

    Update .eslintrc.js

--- react-template/.eslintrc.js
+++ react-template/.eslintrc.js
@@ -21,6 +21,58 @@ module.exports = {
   },
   rules: {
     'prettier/prettier': 'error',
+    const path = require('path');
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: ['react-app', 'plugin:prettier/recommended'],
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-hooks',
+    'simple-import-sort',
+    'import',
+    'jsx-a11y',
+    'html',
+    'prettier',
+  ],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  parserOptions: {
+    // 支持最新 JavaScript
+    project: path.resolve(__dirname, './tsconfig.json'),
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+  },
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
+};
+
   },
   settings: {
     react: {
